4a3caea71a91224d86879ee90d7d54aee79f2739
jaramilloAngie <114117690+jaramilloAngie@users.noreply.github.com>
2024-11-20 14:13:51 -0500

    comentarios
    caja comentarios

--- cliente/src/pages/ProductDetails.js
+++ cliente/src/pages/ProductDetails.js
@@ -1,5 +1,5 @@
 import React, { useCallback, useContext, useEffect, useState } from 'react'
-import  { useNavigate, useParams } from 'react-router-dom'
+import { useNavigate, useParams } from 'react-router-dom'
 import SummaryApi from '../common'
 import { FaStar } from "react-icons/fa";
 import { FaStarHalf } from "react-icons/fa";
@@ -9,39 +9,43 @@ import Context from '../context';
 import displayCOPCurrency from '../helpers/displayCurrency';
 
 const ProductDetails = () => {
-  const [data,setData] = useState({
-    productName : "",
-    brandName : "",
-    category : "",
-    productImage : [],
-    description : "",
-    price : "",
-    sellingPrice : ""
+  const [data, setData] = useState({
+    productName: "",
+    brandName: "",
+    category: "",
+    productImage: [],
+    description: "",
+    price: "",
+    sellingPrice: ""
   })
   const params = useParams()
-  const [loading,setLoading] = useState(true)
+  const [loading, setLoading] = useState(true)
   const productImageListLoading = new Array(4).fill(null)
-  const [activeImage,setActiveImage] = useState("")
+  const [activeImage, setActiveImage] = useState("")
 
-  const [zoomImageCoordinate,setZoomImageCoordinate] = useState({
-    x : 0,
-    y : 0
+  const [zoomImageCoordinate, setZoomImageCoordinate] = useState({
+    x: 0,
+    y: 0
   })
-  const [zoomImage,setZoomImage] = useState(false)
+  const [zoomImage, setZoomImage] = useState(false)
 
   const { fetchUserAddToCart } = useContext(Context)
 
   const navigate = useNavigate()
 
-  const fetchProductDetails = async()=>{
+  const [comentarios, setComentarios] = useState([]);
+  const [nuevoComentario, setNuevoComentario] = useState('');
+  const [nombre, setNombre] = useState('');
+
+  const fetchProductDetails = async () => {
     setLoading(true)
-    const response = await fetch(SummaryApi.productDetails.url,{
-      method : SummaryApi.productDetails.method,
-      headers : {
-        "content-type" : "application/json"
+    const response = await fetch(SummaryApi.productDetails.url, {
+      method: SummaryApi.productDetails.method,
+      headers: {
+        "content-type": "application/json"
       },
-      body : JSON.stringify({
-        productId : params?.id
+      body: JSON.stringify({
+        productId: params?.id
       })
     })
     setLoading(false)
@@ -52,20 +56,20 @@ const ProductDetails = () => {
 
   }
 
-  console.log("data",data)
+  console.log("data", data)
 
-  useEffect(()=>{
+  useEffect(() => {
     fetchProductDetails()
-  },[params])
+  }, [params])
 
-  const handleMouseEnterProduct = (imageURL)=>{
+  const handleMouseEnterProduct = (imageURL) => {
     setActiveImage(imageURL)
   }
 
-  const handleZoomImage = useCallback((e) =>{
+  const handleZoomImage = useCallback((e) => {
     setZoomImage(true)
-    const { left , top, width , height } = e.target.getBoundingClientRect()
-    console.log("coordinate", left, top , width , height)
+    const { left, top, width, height } = e.target.getBoundingClientRect()
+    console.log("coordinate", left, top, width, height)
 
     const x = (e.clientX - left) / width
     const y = (e.clientY - top) / height
@@ -74,171 +78,212 @@ const ProductDetails = () => {
       x,
       y
     })
-  },[zoomImageCoordinate])
+  }, [zoomImageCoordinate])
 
-  const handleLeaveImageZoom = ()=>{
+  const handleLeaveImageZoom = () => {
     setZoomImage(false)
   }
 
 
-  const handleAddToCart = async(e,id) =>{
-    await addToCart(e,id)
+  const handleAddToCart = async (e, id) => {
+    await addToCart(e, id)
     fetchUserAddToCart()
   }
 
-  const handleBuyProduct = async(e,id)=>{
-    await addToCart(e,id)
+  const handleBuyProduct = async (e, id) => {
+    await addToCart(e, id)
     fetchUserAddToCart()
     navigate("/cart")
 
   }
 
+  const agregarComentario = () => {
+    const comentario = {
+      nombre,
+      comentario: nuevoComentario,
+    };
+    setComentarios([...comentarios, comentario]);
+    setNuevoComentario('');
+    setNombre('');
+  };
+
   return (
     <div className='container mx-auto p-4'>
 
       <div className='min-h-[200px] flex flex-col lg:flex-row gap-4 rounded-md p-2 border-2 border-slate-400'>
-          {/***product Image */}
-          <div className='h-96 flex flex-col lg:flex-row-reverse gap-4 '>
+        {/***product Image */}
+        <div className='h-96 flex flex-col lg:flex-row-reverse gap-4 '>
 
-              <div className='h-[300px] w-[300px] lg:h-96 lg:w-96 bg-slate-200 relative p-2 rounded-md border-2 border-red-600'>
-                  <img src={activeImage} className='h-full w-full object-scale-down mix-blend-multiply' onMouseMove={handleZoomImage} onMouseLeave={handleLeaveImageZoom}/>
+          <div className='h-[300px] w-[300px] lg:h-96 lg:w-96 bg-slate-200 relative p-2 rounded-md border-2 border-red-900'>
+            <img src={activeImage} className='h-full w-full object-scale-down mix-blend-multiply' onMouseMove={handleZoomImage} onMouseLeave={handleLeaveImageZoom} />
 
-                    {/**product zoom */}
-                    {
-                      zoomImage && (
-                        <div className='hidden lg:block absolute min-w-[500px] overflow-hidden min-h-[400px] bg-slate-200 p-1 -right-[510px] top-0 '>
-                          <div
-                            className='w-full h-full min-h-[400px] min-w-[500px] mix-blend-multiply scale-150 '
-                            style={{
-                              background : `url(${activeImage})`,
-                              backgroundRepeat : 'no-repeat',
-                              backgroundPosition : `${zoomImageCoordinate.x * 100}% ${zoomImageCoordinate.y * 100}% `
-    
-                            }}
-                          >
-    
-                          </div>
-                        </div>
-                      )
-                    }
-                  
-              </div>
+            {/**product zoom */}
+            {
+              zoomImage && (
+                <div className='hidden lg:block absolute min-w-[500px] overflow-hidden min-h-[400px] bg-slate-200 p-1 -right-[510px] top-0 '>
+                  <div
+                    className='w-full h-full min-h-[400px] min-w-[500px] mix-blend-multiply scale-150 '
+                    style={{
+                      background: `url(${activeImage})`,
+                      backgroundRepeat: 'no-repeat',
+                      backgroundPosition: `${zoomImageCoordinate.x * 100}% ${zoomImageCoordinate.y * 100}% `
 
-              <div className='h-full'>
-                  {
-                    loading ? (
-                      <div className='flex gap-2 lg:flex-col overflow-scroll scrollbar-none h-full '>
-                        {
-                          productImageListLoading.map((el,index) =>{
-                            return(
-                              <div className='h-20 w-20 bg-slate-200 rounded animate-pulse' key={"loadingImage"+index}>
-                              </div>
-                            )
-                          })
-                        }
-                      </div>
-                      
-                    ) : (
-                      <div className='flex gap-2 lg:flex-col overflow-scroll scrollbar-none h-full'>
-                        {
-                          data?.productImage?.map((imgURL,index) =>{
-                            return(
-                              <div className='h-20 w-20 bg-slate-200 p-1 rounded-md border-2 border-red-600' key={imgURL}>
-                                <img src={imgURL} className='w-full h-full object-scale-down mix-blend-multiply cursor-pointer' onMouseEnter={()=>handleMouseEnterProduct(imgURL)}  onClick={()=>handleMouseEnterProduct(imgURL)}/>
-                              </div>
-                            )
-                          })
-                        }
-                      </div>
-                    )
-                  }
-              </div>
+                    }}
+                  >
+
+                  </div>
+                </div>
+              )
+            }
+
           </div>
 
-           {/***product details */}
-           {
-            loading ? (
-              <div className='grid gap-1 w-full'>
-                <p className='bg-slate-200 animate-pulse  h-6 lg:h-8 w-full rounded-full inline-block'></p>
-                <h2 className='text-2xl lg:text-4xl font-medium h-6 lg:h-8  bg-slate-200 animate-pulse w-full'></h2>
-                <p className='capitalize text-slate-400 bg-slate-200 min-w-[100px] animate-pulse h-6 lg:h-8  w-full'></p>
-
-                <div className='text-red-600 bg-slate-200 h-6 lg:h-8  animate-pulse flex items-center gap-1 w-full'>
-    
+          <div className='h-full'>
+            {
+              loading ? (
+                <div className='flex gap-2 lg:flex-col overflow-scroll scrollbar-none h-full '>
+                  {
+                    productImageListLoading.map((el, index) => {
+                      return (
+                        <div className='h-20 w-20 bg-slate-200 rounded animate-pulse' key={"loadingImage" + index}>
+                        </div>
+                      )
+                    })
+                  }
                 </div>
 
-                <div className='flex items-center gap-2 text-2xl lg:text-3xl font-medium my-1 h-6 lg:h-8  animate-pulse w-full'>
-                  <p className='text-red-600 bg-slate-200 w-full'></p>
-                  <p className='text-slate-400 line-through bg-slate-200 w-full'></p>
+              ) : (
+                <div className='flex gap-2 lg:flex-col overflow-scroll scrollbar-none h-full'>
+                  {
+                    data?.productImage?.map((imgURL, index) => {
+                      return (
+                        <div className='h-20 w-20 bg-slate-200 p-1 rounded-md border-2 border-red-900' key={imgURL}>
+                          <img src={imgURL} className='w-full h-full object-scale-down mix-blend-multiply cursor-pointer' onMouseEnter={() => handleMouseEnterProduct(imgURL)} onClick={() => handleMouseEnterProduct(imgURL)} />
+                        </div>
+                      )
+                    })
+                  }
                 </div>
+              )
+            }
+          </div>
+        </div>
 
-                <div className='flex items-center gap-3 my-2 w-full'>
-                  <button className='h-6 lg:h-8  bg-slate-200 rounded animate-pulse w-full'></button>
-                  <button className='h-6 lg:h-8  bg-slate-200 rounded animate-pulse w-full'></button>
-                </div>
+        {/***product details */}
+        {
+          loading ? (
+            <div className='grid gap-1 w-full'>
+              <p className='bg-slate-200 animate-pulse  h-6 lg:h-8 w-full rounded-full inline-block'></p>
+              <h2 className='text-2xl lg:text-4xl font-medium h-6 lg:h-8  bg-slate-200 animate-pulse w-full'></h2>
+              <p className='capitalize text-slate-400 bg-slate-200 min-w-[100px] animate-pulse h-6 lg:h-8  w-full'></p>
 
-                <div className='w-full'>
-                  <p className='text-slate-600 font-medium my-1 h-6 lg:h-8   bg-slate-200 rounded animate-pulse w-full'></p>
-                  <p className=' bg-slate-200 rounded animate-pulse h-10 lg:h-12  w-full'></p>
-                </div>
+              <div className='text-red-900 bg-slate-200 h-6 lg:h-8  animate-pulse flex items-center gap-1 w-full'>
+
               </div>
-            ) : 
+
+              <div className='flex items-center gap-2 text-2xl lg:text-3xl font-medium my-1 h-6 lg:h-8  animate-pulse w-full'>
+                <p className='text-red-900 bg-slate-200 w-full'></p>
+                <p className='text-slate-400 line-through bg-slate-200 w-full'></p>
+              </div>
+
+              <div className='flex items-center gap-3 my-2 w-full'>
+                <button className='h-6 lg:h-8  bg-slate-200 rounded animate-pulse w-full'></button>
+                <button className='h-6 lg:h-8  bg-slate-200 rounded animate-pulse w-full'></button>
+              </div>
+
+              <div className='w-full'>
+                <p className='text-slate-600 font-medium my-1 h-6 lg:h-8   bg-slate-200 rounded animate-pulse w-full'></p>
+                <p className=' bg-slate-200 rounded animate-pulse h-10 lg:h-12  w-full'></p>
+              </div>
+            </div>
+          ) :
             (
               <div className='flex flex-col gap-1 '>
                 {data?.brandName && (
-  <p className='bg-red-200 text-red-600 px-2 rounded-full inline-block w-fit '>
-    {data.brandName}
-  </p>
-)}
+                  <p className='bg-gray-300 text-red-900 px-2 rounded-full inline-block w-fit '>
+                    {data.brandName}
+                  </p>
+                )}
                 <h2 className='text-2xl lg:text-4xl font-medium'>{data?.productName}</h2>
                 <p className='capitalize text-slate-400'>{data?.category}</p>
 
-                <div className='text-red-600 flex items-center gap-1'>
-                    <FaStar/>
-                    <FaStar/>
-                    <FaStar/>
-                    <FaStar/>
-                    <FaStarHalf/>
+                <div className='text-red-900 flex items-center gap-1'>
+                  <FaStar />
+                  <FaStar />
+                  <FaStar />
+                  <FaStar />
+                  <FaStarHalf />
                 </div>
 
                 <div className='flex items-center gap-2 text-2xl lg:text-3xl font-medium my-1'>
-                  <p className='text-red-600'>{displayCOPCurrency(data.sellingPrice)}</p>
+                  <p className='text-red-900'>{displayCOPCurrency(data.sellingPrice)}</p>
                   {/* Solo mostrar el precio tachado si el precio normal es diferente al precio de venta */}
-        { data?.price !== data?.sellingPrice && (
-            <p className='text-slate-500 line-through'>{ displayCOPCurrency(data?.price) }</p>
-        )}
+                  {data?.price !== data?.sellingPrice && (
+                    <p className='text-slate-500 line-through'>{displayCOPCurrency(data?.price)}</p>
+                  )}
                 </div>
 
                 <div className='flex items-center gap-3 my-2'>
-                  <button className='border-2 border-red-600 rounded px-3 py-1 min-w-[120px] text-red-600 font-medium hover:bg-red-600 hover:text-white' onClick={(e)=>handleBuyProduct(e,data?._id)}>Comprar</button>
-                  <button className='border-2 border-red-600 rounded px-3 py-1 min-w-[120px] font-medium text-white bg-red-600 hover:text-red-600 hover:bg-white' onClick={(e)=>handleAddToCart(e,data?._id)}>Añadir al carrito</button>
+                  <button className='border-2 border-red-900 rounded px-3 py-1 min-w-[120px] text-red-900 font-medium hover:bg-red-900 hover:text-white' onClick={(e) => handleBuyProduct(e, data?._id)}>Comprar</button>
+                  <button className='border-2 border-red-900 rounded px-3 py-1 min-w-[120px] font-medium text-white bg-red-900 hover:text-red-900 hover:bg-white' onClick={(e) => handleAddToCart(e, data?._id)}>Añadir al carrito</button>
                 </div>
 
                 <div>
-                  
+
                   {data?.description !== "" && (
                     <p className='text-slate-600 font-medium my-1'>Descripción :</p>
- 
-)}
- <p>{data.description}</p>
+
+                  )}
+                  <p>{data.description}</p>
                 </div>
               </div>
             )
-           }
+        }
 
       </div>
 
-
-
+      <section class="comentarios">
+        <h1 class="heading">Comentarios</h1>
+        <div className='min-h-[200px] flex flex-col lg:flex-row gap-4 rounded-md p-2 border-2 border-slate-400'>
+          <div className="flex flex-col gap-4">
+            {comentarios.map((comentario, index) => (
+              <div key={index} className="flex gap-4">
+                <div className="bg-gray-200 border-2 border-dashed rounded-xl w-16 h-16" />
+                <div className="flex flex-col">
+                  <span className="text-lg font-bold">{comentario.nombre}</span>
+                  <span className="text-gray-600">{comentario.comentario}</span>
+                </div>
+              </div>
+            ))}
+          </div>
+          <div className="flex flex-col gap-4 mt-4">
+            <input
+              type="text"
+              placeholder="Nombre"
+              value={nombre}
+              onChange={(e) => setNombre(e.target.value)}
+              className="p-2 border border-gray-300 rounded-lg"
+            />
+            <textarea
+              placeholder="Comentario"
+              value={nuevoComentario}
+              onChange={(e) => setNuevoComentario(e.target.value)}
+              className="p-2 border border-gray-300 rounded-lg"
+            />
+            <button
+              onClick={agregarComentario}
+              className="bg-red-900 hover:bg-red-800 text-white font-bold py-2 px-4 rounded-lg"
+            >
+              Agregar comentario
+            </button>
+          </div>
+        </div>
+      </section>
       {
         data.category && (
-          <CategroyWiseProductDisplay category={data?.category} heading={"Productos Relacionados"}/>
+          <CategroyWiseProductDisplay category={data?.category} heading={"Productos Relacionados"} />
         )
       }
-     
-
-
-
     </div>
   )
 }
